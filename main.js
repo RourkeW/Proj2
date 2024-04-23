ec2dbbe63327c66f496b7089a0132ed9ab4f47d3
Rourke Whitcombe <rourke28whit@gmail.com>
2024-04-23 23:07:13 +0000

    feat: i added some java that will add 9 div tags numbered in my board div

---
+++ main.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    runGame();
+}
+
+function runGame() {
+    for (let i = 0; i < 9; i++) {
+        //this is creating my 9 divs in the js withing the big game border I made in html you can see them now if we went to inspect
+        let tile = document.createElement("div");
+        tile.id=i.toString();
+        document.getElementById("board").appendChild(tile);
+    }
+}
